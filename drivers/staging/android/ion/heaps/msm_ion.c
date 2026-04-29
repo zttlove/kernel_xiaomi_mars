6e8ab2c67f0af4579d4a706ca1d118c6af9ddee6
zttlove <115808960+zttlove@users.noreply.github.com>
2026-04-29 10:26:49 +0800

    Update msm_ion.c

--- drivers/staging/android/ion/heaps/msm_ion.c
+++ drivers/staging/android/ion/heaps/msm_ion.c
@@ -250,9 +250,9 @@ bool msm_ion_heap_is_secure(int heap_id)
 {
 	struct ion_heap *heap = ion_heap_by_id(heap_id);
 
-	if (IS_ERR(heap) || !(heap->type == ION_HEAP_TYPE_SECURE_CARVEOUT ||
-			      heap->type == ION_HEAP_TYPE_SYSTEM_SECURE ||
-			      heap->type == ION_HEAP_TYPE_HYP_CMA))
+	if (IS_ERR(heap) || !((int)heap->type == ION_HEAP_TYPE_SECURE_CARVEOUT ||
+                       (int)heap->type == ION_HEAP_TYPE_SYSTEM_SECURE ||
+                       (int)heap->type == ION_HEAP_TYPE_HYP_CMA))
 		return false;
 
 	return true;
